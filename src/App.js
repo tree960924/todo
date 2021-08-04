1bb5cae17c35ac4979e44064815024ab32983d87
Song <9901590@naver.com>
2021-08-04 20:22:24 +0900

    3일차 커밋
    변경 사항
    1. Component 클래스 구상
    2. 모든 컴포넌트들에 Component 클래스 상속

--- src/App.js
+++ src/App.js
@@ -1,39 +1,80 @@
-class App{
+class Component{
+	constructor(parent, props){
+		this.parent = parent;
+        if(this.parent !== undefined){
+            this.$parent = this.parent.$target;
+            this.parent.components.push(this);
+        }
+		this.components = [];
+		this.props = props;
+        this.state = {};
+	}
+	
+	setState = (newState) => {
+		this.state = {...this.state, ...newState}
+		if(this.components.length > 0){
+			for(let component of this.components){
+				component.update();
+			}
+		}
+		this.render();
+	}
+
+	setProps = (newProps) => {
+		this.props = {...this.props, ...newProps};
+		this.render();
+	}
+
+    render(){}
+
+	update(){
+		//각 컴포넌트들에서 구현
+		//부모의 클래스를 참조하여 필요한 값을 가져오는 풀링 방식
+		//가져온 이후 setProps로 props 갱신
+	}
+}
+	
+
+class App extends Component{
     constructor(){
+        super();
         this.$app = document.querySelector('.app');
         this.$target = document.createElement('div');
         this.$target.className = 'todo_container';
         this.$app.appendChild(this.$target);
         this.state = {
             todoList : ['밥먹기', '일찍 자기', '공부하기'],
-            memoList : ['알차게 밥먹기', '알차게 일찍 자기', '알차게 공부하기']
+            memoList : ['알차게 밥먹기', '알차게 일찍 자기', '알차게 공부하기'],
+            selectedMemo : ''
         }
-        this.todoListView = new TodoListView(this.$target, this.state.todoList, this.onSelect);
-        this.memoView = new MemoView(this.$target);
-        this.todoInput = new TodoInput(this.$target, this.onAdd);
+        this.todoListView = new TodoListView(this, {
+            todoList : this.state.todoList, 
+            onSelect : this.onSelect
+        });
+        this.memoView = new MemoView(this, {
+            memo : this.state.selectedMemo
+        });
+        this.todoInput = new TodoInput(this, {
+            onAdd : this.onAdd
+        });
     }
    
     onSelect = (index)=>{
-        console.log(index);
-        this.memoView.setState({memo : this.state.memoList[index]})
+        this.setState({selectedMemo : this.state.memoList[index]});
     }
 
     onAdd = (todo, memo)=>{
-        this.state.todoList.push(todo);
-        this.state.memoList.push(memo);
-        console.log(this.state.todoList);
-        console.log(this.state.memoList);
-        this.todoListView.setState({todoList:this.state.todoList});
+        this.setState({
+            todoList : [...this.state.todoList, todo],
+            memoList : [...this.state.memoList, memo]
+        })
+        console.log(this.state);
     }
 }
 
-class TodoListView{
-   constructor($parent, todoList, onSelect){
-       this.$parent = $parent;
-       this.state = {
-           todoList : todoList
-       };
-       this.onSelect = onSelect;
+class TodoListView extends Component{
+   constructor(parent, props){
+       super(parent, props);
        this.$target = document.createElement('ul');
        this.$target.className = 'todo_list_view';
        this.$parent.appendChild(this.$target);
@@ -42,15 +83,14 @@ class TodoListView{
    }
 
    render(){
-       if(this.state.todoList === undefined){
+       if(this.props.todoList === undefined){
            this.$target.innerHTML = `
            등록된 Todo가 없습니다.
            `
        }
        else{
-           console.log(this.state.todoList);
-           this.$target.innerHTML = this.state.todoList.map((element, index)=>
-               `<li data-index="${index}">
+           this.$target.innerHTML = this.props.todoList.map((element, index)=>
+               `<li data-index="${index}" ${index === this.state.selectedIndex && "selected"}>
                     <input id="todo${index}" type="checkbox">
                     <label for="todo${index}">${element}</label>         
                 </li>`
@@ -60,58 +100,54 @@ class TodoListView{
 
    setEvent(){
        this.$target.addEventListener('click', (e)=>{
-           let index = e.target.dataset.index;
-           if(index){
-               let prevSelected = this.$target.querySelector('li[selected]');
-               if(prevSelected){
-                   prevSelected.removeAttribute('selected');
-               }
-               e.target.setAttribute('selected', '');
-               this.onSelect(index);
+           let index = e.target.dataset.index*1;
+           if(index >= 0){
+               this.props.onSelect(index);
+               this.setState({selectedIndex : index});
+               console.log(this.state);
            }
        })
    }
 
-   setState = (newState)=>{
-        this.state = {...this.state, ...newState};
-        this.render();
-    }
+   update(){
+       console.log('todoListView update called');
+       this.setProps({todoList : this.parent.state.todoList});
+   }
 }
 
-class MemoView{
-    constructor($parent, memo){
-        this.$parent = $parent;
+class MemoView extends Component{
+    constructor(parent, props){
+        super(parent, props);
         this.$target = document.createElement('div');
-        this.state = {};
-        this.state.memo = memo;
         this.$target.className = 'memo_view';
         this.$parent.appendChild(this.$target);
         this.render();
     }
 
     render(){
-        if(this.state.memo === undefined){
+        if(this.props.memo === undefined){
             this.$target.innerHTML = '';
         }
         else{
-            this.$target.innerHTML = this.state.memo;
+            this.$target.innerHTML = this.props.memo;
         }
     }
 
-    setState = (newState)=>{
-        this.state = {...this.state, ...newState};
-        this.render();
+    update(){
+        this.setProps({memo : this.parent.state.selectedMemo});
     }
 }
 
-class TodoInput{
-    constructor($parent, onAdd){
-        this.$parent = $parent;
-        this.onAdd = onAdd;
+class TodoInput extends Component{
+    constructor(parent, props){
+        super(parent, props);
         this.$target = document.createElement('div');
         this.$target.className = 'todo_input';
-        this.state = {};
         this.$parent.appendChild(this.$target);
+        this.state = {
+            todo : '',
+            memo : ''
+        };
         this.render();
         this.setEvent();
     }
@@ -119,11 +155,11 @@ class TodoInput{
     render(){
         this.$target.innerHTML = `
             <div class="input_group">
-                <input class="input_todo" name="todo" type="text" required>
+                <input class="input_todo" name="todo" value = "${this.state.todo}"type="text" required>
                 <span class="bar"></span>
                 <label>title</label>
             </div>
-            <textarea class = "input_memo" name="memo" placeholder="context"></textarea>
+            <textarea class = "input_memo" name="memo" placeholder="context">${this.state.memo}</textarea>
             <button class="btn btn_add">+ add</button>
         `
     }
@@ -131,43 +167,16 @@ class TodoInput{
     setEvent(){
         this.$target.addEventListener('click', (e)=>{
             if(e.target.classList.contains('btn_add')){
-                console.log(this);
-                this.onAdd(this.state.todo, this.state.memo);
-                
+                this.props.onAdd(this.state.todo, this.state.memo);
+                this.setState({todo : '', memo : ''});
             }
         })
         this.$target.addEventListener('change', (e)=>{
-            console.log(e.target);
             let inputcontent = e.target.name;
+            //this.setState({[inputcontent] : e.target.value});
             this.state[inputcontent] = e.target.value;
+            console.log(this.state);
         })
-    }
-}
-
-class Component{
-    constructor($parent, props){
-        this.$parent = $parent;
-        this.props = props;
-    }
-
-    setState = (newState)=>{
-        this.state = {...this.state, ...newState};
-        if(this.render !== undefined){
-            this.render();
-        }
-        if(this.subComponent !== undefined){
-            for(component in this.subComponent){
-                this.subComponent[component].setProps(this.state);
-            }
-        }
-    }
-
-    setProps = (newProps) => {
-        this.props = {...this.props, ...newProps};
-
-        if(this.render !== undefined){
-            this.render();
-        }
     }
 }
 
